ca1483e5f7aaa929b447e8eaf5488e7df6492020
wfchou <chou.weifong@gmail.com>
2017-03-08 17:15:13 -0500

    updated database

--- Songbird/database/tbl_create.sql
+++ Songbird/database/tbl_create.sql
@@ -1,10 +1,16 @@
 CREATE TABLE sampleInfo (
 	sampleid VARCHAR(20) PRIMARY KEY,
-	date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	added TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	date TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00',
 	samplelength INTEGER,
 	format VARCHAR(10),
 	latitude VARCHAR(20),
 	longitude VARCHAR(20),
 	humidity INTEGER,
-	temp INTEGER
+	temp INTEGER,
+	light VARCHAR(10),
+	mfcc VARCHAR(255),
+	species1 INTEGER,
+	species2 INTEGER,
+	species3 INTEGER
 );
