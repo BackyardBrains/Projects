9f4c410e83c2b011cc72a809c0b45460c7a6be4a
wfchou <chou.weifong@gmail.com>
2017-03-11 17:08:22 -0500

    database update to the species and percentage and the variable types

--- Songbird/database/tbl_create.sql
+++ Songbird/database/tbl_create.sql
@@ -1,16 +1,15 @@
 CREATE TABLE sampleInfo (
 	sampleid VARCHAR(20) PRIMARY KEY,
 	added TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	date TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00',
-	samplelength INTEGER,
-	format VARCHAR(10),
-	latitude VARCHAR(20),
-	longitude VARCHAR(20),
+	latitude DECIMAL,
+	longitude DECIMAL,
 	humidity INTEGER,
-	temp INTEGER,
-	light VARCHAR(10),
-	mfcc VARCHAR(255),
-	species1 INTEGER,
-	species2 INTEGER,
-	species3 INTEGER
+	temp DECIMAL,
+	light DECIMAL,
+	type1 VARCHAR(20),
+	per1 DECIMAL,
+	type2 VARCHAR(20),
+	per2 DECIMAL,
+	type3 VARCHAR(20),
+	per3 DECIMAL
 );
