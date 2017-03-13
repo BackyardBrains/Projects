0690d9fb6cd4c4bd1f24e86be821e62b45a94534
zhuangjiadi <yuantian@0587368884.wireless.umich.net>
2017-03-13 10:54:32 -0400

    -a

--- Songbird/database/controllers/main.py
+++ Songbird/database/controllers/main.py
@@ -3,19 +3,25 @@ from flask import *
 from extensions import connect_to_database
 from flask import url_for
 
-main = Blueprint('main', __name__, template_folder='templates',url_prefix='/panhxxwm/p1/')
+main = Blueprint('main', __name__, template_folder='templates')
 
-@main.route('/')
+db = connect_to_database()
+
+@main.route('/', methods = ['GET', 'POST'])
 def main_route():
     db = connect_to_database()
     cur = db.cursor()
     
-    if request.method == 'GET':
+    result = ''
+    if request.method == 'POST':
         button = request.form.get('op')
+        print("button: ", button)
 
-        cur.execute("SELECT * FROM sampleInfo ORDER BY %s;", button)
+        cur = db.cursor()
+        cur.execute("SELECT * FROM sampleInfo ORDER BY %s", (button, ))
+        result = cur.fetchall()
 
     options = {
-        
+        "result": result
     }
     return render_template("index.html", **options)
---
+++ Songbird/database/load_data.sql
@@ -0,0 +1,7 @@
+INSERT INTO sampleInfo (sampleid, deviceid, added, latitude, longitude, humidity, temp, light, type1, per1, type2, per2, type3, per3)
+                       VALUES
+                       (1, 1, CURRENT_TIMESTAMP, 10, 50, 30, 20, 40, "a", 10, "aa", 19, "aaa", 38),
+                       (2, 2, CURRENT_TIMESTAMP, 20, 40, 10, 30, 50, "b", 40, "bb", 59, "bbb", 18),
+                       (3, 3, CURRENT_TIMESTAMP, 30, 30, 20, 10, 40, "c", 30, "cc", 39, "ccc", 58),
+                       (4, 4, CURRENT_TIMESTAMP, 40, 20, 50, 40, 10, "d", 20, "dd", 29, "ddd", 28),
+                       (5, 5, CURRENT_TIMESTAMP, 50, 10, 40, 50, 30, "e", 50, "ee", 49, "eee", 48);
