014a53b02a512ad88c4956d1cd7607aab54e4fcc
wfchou <chou.weifong@gmail.com>
2017-03-13 11:01:11 -0400

    options

--- Songbird/database/controllers/main.py
+++ Songbird/database/controllers/main.py
@@ -13,12 +13,41 @@ def main_route():
     cur = db.cursor()
     
     result = ''
+    button = ''
     if request.method == 'POST':
-        button = request.form.get('op')
+        if (request.form.get('op') == 'sampleid'):
+            button = 'sampleid'
+        elif (request.form.get('op') == 'deviceid'):
+            button = 'deviceid'
+        elif (request.form.get('op') == 'added'):
+            button = 'added'
+        elif (request.form.get('op') == 'latitude'):
+            button = 'latitude'
+        elif (request.form.get('op') == 'longitude'):
+            button = 'longitude'
+        elif (request.form.get('op') == 'humidity'):
+            button = 'humidity'
+        elif (request.form.get('op') == 'temp'):
+            button = 'temp'
+        elif (request.form.get('op') == 'light'):
+            button = 'light'
+        elif (request.form.get('op') == 'type1'):
+            button = 'type1'
+        elif (request.form.get('op') == 'per1'):
+            button = 'per1'
+        elif (request.form.get('op') == 'type2'):
+            button = 'type2'
+        elif (request.form.get('op') == 'per2'):
+            button = 'per2'
+        elif (request.form.get('op') == 'type3'):
+            button = 'type3'
+        elif (request.form.get('op') == 'per3'):
+            button = 'per3'
+
         print("button: ", button)
 
         cur = db.cursor()
-        cur.execute("SELECT * FROM sampleInfo ORDER BY %s", (button, ))
+        cur.execute("SELECT * FROM sampleInfo ORDER BY %s", %(button))
         result = cur.fetchall()
 
     options = {
