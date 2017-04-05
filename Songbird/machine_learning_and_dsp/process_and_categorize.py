e069722d485390cebaba394aa8809d1852f984d1
znrobins <znrobins@umich.edu>
2017-04-05 19:29:22 -0400

    added categorize file, parrellelized noisered

--- Songbird/machine_learning_and_dsp/process_and_categorize.py
+++ Songbird/machine_learning_and_dsp/process_and_categorize.py
@@ -2,18 +2,31 @@
 
 
 import getopt
+import glob
 import os
 import sys
+
+import MySQLdb
+from pyAudioAnalysis import audioTrainTest as aT
+
+from config import *
+
+
+def classify(directory=os.getcwd(), model_file=os.path.join(os.getcwd(), 'model'), classifierType='gradientboosting'):
+    for file in glob.glob("*.wav"):
+        Result, P, classNames = aT.fileClassification(file, model_file, classifierType)
+        with MySQLdb.connect(host=host, user=user, passwd=passwd,
+                             db=database) as cur:  # config is in config.py: see above
+            cur.execute("insert ")  # .........
 
 if __name__ == '__main__':
 
     directory = os.getcwd()
     classifierType = 'gradientboosting'
-    birds = []
     verbose = False
 
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "d:m:c:b:v", ["dir=", "model=", "classifier=", "bird=", "verbose"])
+        opts, args = getopt.getopt(sys.argv[1:], "d:m:c:v", ["dir=", "model=", "classifier=", "verbose"])
     except getopt.GetoptError as err:
         # print help information and exit:
         print str(err)  # will print something like "option -a not recognized"
@@ -25,8 +38,6 @@ if __name__ == '__main__':
             model_file = arg
         elif opt in ("-c", "--classifier"):
             classifierType = arg
-        elif opt in ("-b", "--bird"):
-            birds.append(arg)
         elif opt in ("-v", "--verbose"):
             verbose = True
         else:
