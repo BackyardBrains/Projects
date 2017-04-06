c97ae345697fc1dcb062ceccd91e3fa31746d8a0
znrobins <znrobins@umich.edu>
2017-04-06 13:40:21 -0400

    added skip-clean option to clean and test and fixed model name bug

--- Songbird/machine_learning_and_dsp/clean_and_test.py
+++ Songbird/machine_learning_and_dsp/clean_and_test.py
@@ -20,7 +20,7 @@ def test_params(dir, categories):
     return test_dirs
 
 
-def clean_and_test(directory, model_file, classifierType, birds, verbose):
+def clean_and_test(directory, model_file, classifierType, birds, verbose, skip_clean):
     if not len(birds):
         raise Exception("Must specify at least one folder/category to test!")
 
@@ -35,12 +35,15 @@ def clean_and_test(directory, model_file, classifierType, birds, verbose):
 
     try:
         sanatize_filenames(directory, verbose=verbose)
-        for dir in test_dirs:
-            rootdir, subdir = os.path.split(dir)
-            cleaner = noiseCleaner(verbose=verbose)
-            cleaner.noise_removal_dir(rootdir)
+        if not skip_clean:
+            for dir in test_dirs:
+                rootdir, subdir = os.path.split(dir)
+                cleaner = noiseCleaner(verbose=verbose)
+                cleaner.noise_removal_dir(rootdir)
         model_dir, model_name = os.path.split(model_file)
-        test_model(test_dirs, modelName=model_name, model_dir=directory, classifierType=classifierType,
+        if not len(model_dir):
+            model_dir = os.getcwd()
+        test_model(test_dirs, modelName=model_name, model_dir=model_dir, classifierType=classifierType,
                    store_to_mySQL=True, verbose=verbose)
     except Exception:
         send_notification("Clean and test process failed.")
@@ -57,9 +60,11 @@ if __name__ == '__main__':
     birds = []
     verbose = False
     model_file = os.path.join(directory, 'model')
+    skip_clean = False
 
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "d:m:c:b:v", ["dir=", "model=", "classifier=", "bird=", "verbose"])
+        opts, args = getopt.getopt(sys.argv[1:], "d:m:c:b:vs",
+                                   ["dir=", "model=", "classifier=", "bird=", "verbose", "skip-clean"])
     except getopt.GetoptError as err:
         # print help information and exit:
         print str(err)  # will print something like "option -a not recognized"
@@ -75,6 +80,8 @@ if __name__ == '__main__':
             birds.append(arg)
         elif opt in ("-v", "--verbose"):
             verbose = True
+        elif opt in ("-s", "--skip-clean"):
+            skip_clean = True
         else:
             assert False, "unhandled option"
 
@@ -84,4 +91,4 @@ if __name__ == '__main__':
     if classifierType not in ('knn', 'svm', 'gradientboosting', 'randomforest', 'extratrees'):
         raise Exception(classifierType + " is not a valid model type!")
 
-    clean_and_test(directory, model_file, classifierType, birds, verbose=verbose)
+    clean_and_test(directory, model_file, classifierType, birds, verbose=verbose, skip_clean=skip_clean)
