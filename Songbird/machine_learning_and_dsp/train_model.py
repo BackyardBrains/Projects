17c0ceea75459016a0707df238d713009a6d02bb
znrobins <znrobins@umich.edu>
2017-04-27 15:12:18 -0400

    parallelized testing procedure

--- Songbird/machine_learning_and_dsp/train_model.py
+++ Songbird/machine_learning_and_dsp/train_model.py
@@ -1,12 +1,11 @@
-import os
-
 from pyAudioAnalysis import audioTrainTest
 
-from test_model import test_model
+from test_model import tester
 
 
 def train_model(list_of_dirs, mtWin=1.0, mtStep=1.0, stWin=audioTrainTest.shortTermWindow,
-                stStep=audioTrainTest.shortTermStep, classifierType='svm', modelName='Test', useBeatmap=False):
+                stStep=audioTrainTest.shortTermStep, classifierType='gradientboosting', modelName='Test',
+                useBeatmap=False):
     # Trains a new classification model see audioTrainTest.py in pyaudioanalysis library for details
     audioTrainTest.featureAndTrain(list_of_dirs, mtWin, mtStep, stWin, stStep, classifierType,
                                    '_'.join([classifierType, modelName]), useBeatmap)
@@ -14,9 +13,9 @@ def train_model(list_of_dirs, mtWin=1.0, mtStep=1.0, stWin=audioTrainTest.shortT
 
 def train_and_test(list_of_dirs, test_dirs, mtWin=1.0, mtStep=1.0,
                    stWin=audioTrainTest.shortTermWindow,
-                   stStep=audioTrainTest.shortTermStep, classifierType='svm', modelName='svmTest', useBeatmap=False,
-                   store_to_mySQL=False):
+                   stStep=audioTrainTest.shortTermStep, classifierType='gradientboosting',
+                   modelName='gradientboostingTest', useBeatmap=False):
     # automatically trains a new model and then tests it using a seperate dataset, see test_model.py
     train_model(list_of_dirs, mtWin, mtStep, stWin, stStep, classifierType, modelName, useBeatmap)
-    test_model(test_dirs, os.getcwd(), modelName, classifierType, store_to_mySQL=store_to_mySQL)
+    t1 = tester(test_dirs, modelName=modelName, classifierType=classifierType)
     # send_notification('test and train complete') #sms notification; see twillio_test.py
