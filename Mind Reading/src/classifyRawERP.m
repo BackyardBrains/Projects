bf0c4b6a66338fc6341ed009726d7b76d457099f
stanislavmircic <stanislavmircic@gmail.com>
2017-07-16 21:53:48 +0200

    Update label on confusion matrix

--- Mind Reading/src/classifyRawERP.m
+++ Mind Reading/src/classifyRawERP.m
@@ -1,11 +1,15 @@
 function [ out] = classifyRawERP( data )
 
+%make training set
 trainingSet = [data.trainingOutputs data.trainingInputs];
-
+%train SVM
 [trainedClassifier, validationAccuracy] = linearRawSVM(trainingSet);
 validationAccuracy
+
+%predict output for test data
 predictedOutputs = trainedClassifier.predictFcn(data.testInputs);
 
+%calculate outputs for test data
 accuracyOnTestData = 1- sum(abs(predictedOutputs - data.testOutputs))/length(predictedOutputs)
 
-plotconfusion(data.testOutputs',predictedOutputs')
+plotconfusion(data.testOutputs',predictedOutputs', 'Face[1] / Non-Face[0] Classification ');
